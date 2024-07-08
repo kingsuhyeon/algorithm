753a3ba1b2aac27867023233ad028733b4b6f5d7
kingsuhyeon <dvlproh@gmail.com>
2024-07-08 23:08:32 +0900

    first commit

---
+++ baekjoon/src/baekjoon_18108.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class baekjoon_18108 {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int bul = 543;
+        int a = sc.nextInt();
+        System.out.println(a - bul);
+    }
+}
