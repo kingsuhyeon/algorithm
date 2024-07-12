96780df5555db0f58deedb82026d539cd5247e2c
kingsuhyeon <dvlproh@gmail.com>
2024-07-12 14:21:46 +0900

    Baekjoon

--- baekjoon/src/level_2/Baekjoon_2884.java
+++ baekjoon/src/level_2/Baekjoon_2884.java
@@ -1,8 +1,24 @@
 package level_2;
 
+import java.util.Scanner;
+
 public class Baekjoon_2884 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
+        if (b >= 45) {
+            b = b - 45;
+        } else {
+            b = b + 15;
+            if (a == 0) {
+                a = 23;
+            } else {
+                a--;
+            }
+        }
+        System.out.println(a + " " + b);
     }
 }
