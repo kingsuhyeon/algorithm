70c07dcffcf103904513b4c973d29010257bff39
kingsuhyeon <dvlproh@gmail.com>
2024-07-13 17:43:59 +0900

    Baekjoon

---
+++ baekjoon/src/level_3/Baekjoon_2439.java
@@ -0,0 +1,21 @@
+package level_3;
+
+import java.util.Scanner;
+
+public class Baekjoon_2439 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= a - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
